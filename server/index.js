a27d5119862dc3a4977454ebf784714f8811a862
Anand <131908427+anandj1@users.noreply.github.com>
2025-03-28 19:43:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ const corsOptions = {
   origin: ['https://chat-code-eta.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'], // Add "Cache-Control"
   exposedHeaders: ['Authorization'],
   preflightContinue: false,
   optionsSuccessStatus: 204
